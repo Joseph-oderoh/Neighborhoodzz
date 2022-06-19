74ffef746fbfbe1609d97ad78f300b2f090ee104
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-19 19:59:45 +0300

    add styling profile navbar and single_hood .htmls files

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -96,7 +96,7 @@ def join_hood(request, id):
     neighbourhood = get_object_or_404(NeighbourHood, id=id)
     request.user.profile.neighbourhood = neighbourhood
     request.user.profile.save()
-    return redirect('hood')
+    return redirect('')
 
 
 def leave_hood(request, id):
