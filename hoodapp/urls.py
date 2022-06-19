a93311a596d78bfa0e8c26c4bb1ded342cf6e710
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-19 11:26:30 +0300

    st:add create hood forms template and view function

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, re_path
 urlpatterns = [
     path('', views.homepage, name='landing'),
     path('hoodz/', views.hoods, name='hood'),
+    path('new_hoodz/', views.create_hood, name='new-hood'),
 ]
--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+
+from .forms import NeighbourHoodForm
 
 from .models import NeighbourHood
 # Create your views here.
@@ -15,3 +17,15 @@ def hoods(request):
         'all_hoods': all_hoods,
     }
     return render(request, 'hoodz.html', params)
+
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'new_hoodz.html', {'form': form})
