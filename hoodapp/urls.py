8e8bbf41c307c1684b9708fd8a41395e9552498e
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-06-20 06:50:08 +0300

    modified procfile

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('single_hood/<int:hood_id>', views.single_hood, name='single-hood'),
     path('<hood_id>/new-post', views.create_post, name='post'),
     path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
 ]
